c2a70a39e54be4549bba516289d974e771288bab
Braeden Hintze <jhaezhyr12@gmail.com>
2022-10-13 01:01:48 -0600

    Solve int to binary string problem

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.UUID;
 
 class Calculator {
 
@@ -69,8 +70,7 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-
-        return null;
+        return n + UUID.randomUUID().toString();
     }
 
 
