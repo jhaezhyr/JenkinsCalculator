0b88abf67fc8daf1b864d05c8efbe48c5801f36b
Braeden Hintze <jhaezhyr12@gmail.com>
2022-10-22 00:53:31 -0600

    Fix it all

---
+++ src/main/java/Main.java
@@ -0,0 +1,50 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Usage: operation left right");
+        }
+        Scanner reader = new Scanner(System.in);
+        String op = reader.next();
+        int l = reader.nextInt();
+        int r = reader.nextInt();
+        Calculator c = new Calculator();
+        int answer = 0;
+        if (op.equals("plus")) {
+            answer = c.add(l,r);
+//            Matthew Gabbitas
+//            Tasibbag thewtam;
+//            Gwen Beatty;
+//            Ytateb nweg;
+//            Ian Goodwin;
+//            windoog nai;
+//            Freshavacado
+        }
+        else if (op.equals("minus")) {
+            answer = c.subtract(l,r);
+        }
+        else if (op.equals("subtract")) {
+            answer = c.subtract(l,r);
+        }
+        else if (op.equals("fib")) {
+            answer = c.fibonacciNumberFinder(l);
+        }
+        else if (op.equals("divide")) {
+            answer = c.divide(l,r);
+        }
+        else if (op.equals("multiply")) {
+            answer = c.multiply(l, r);
+        }
+        else if (op.equals("bin")) {
+            System.out.println(c.intToBinaryNumber(l));
+            return;
+        }
+        else {
+            System.out.println("Bad idea man.");
+            return;
+        }
+        System.out.println("" + answer);
+    }
+}
