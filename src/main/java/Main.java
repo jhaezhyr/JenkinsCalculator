0029eaadeb196d90f42fdca6a2ccbcecf9036809
Braeden Hintze <jhaezhyr12@gmail.com>
2022-10-22 20:44:08 -0600

    Add command line loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,52 @@
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length != 3) {
-            System.out.println("Usage: operation left right");
-        }
         Scanner reader = new Scanner(System.in);
-        String op = reader.next();
-        int l = reader.nextInt();
-        int r = reader.nextInt();
         Calculator c = new Calculator();
-        int answer = 0;
-        if (op.equals("plus")) {
-            answer = c.add(l,r);
-//            Matthew Gabbitas
-//            Tasibbag thewtam;
-//            Gwen Beatty;
-//            Ytateb nweg;
-//            Ian Goodwin;
-//            windoog nai;
-//            Freshavacado
+        while (true) {
+            try {
+                String op = reader.next();
+                int l = reader.nextInt();
+                int r = reader.nextInt();
+                int answer = 0;
+                switch (op) {
+                    case "plus":
+                        answer = c.add(l, r);
+                        //            Matthew Gabbitas
+                        //            Tasibbag thewtam;
+                        //            Gwen Beatty;
+                        //            Ytateb nweg;
+                        //            Ian Goodwin;
+                        //            windoog nai;
+                        //            Freshavacado
+                        break;
+                    case "subtract":
+                        answer = c.subtract(l, r);
+                        break;
+                    case "fib":
+                        answer = c.fibonacciNumberFinder(l);
+                        break;
+                    case "divide":
+                        answer = c.divide(l, r);
+                        break;
+                    case "multiply":
+                        answer = c.multiply(l, r);
+                        break;
+                    case "bin":
+                        System.out.println(c.intToBinaryNumber(l));
+                        continue;
+                    case "exit":
+                        System.out.println("See yah!");
+                        return;
+                    default:
+                        System.out.println("Bad idea man.");
+                        continue;
+                }
+                System.out.println("" + answer);
+            } catch (Exception ex) {
+                System.out.println("Usage: operation left right");
+            }
         }
-        else if (op.equals("minus")) {
-            answer = c.subtract(l,r);
-        }
-        else if (op.equals("subtract")) {
-            answer = c.subtract(l,r);
-        }
-        else if (op.equals("fib")) {
-            answer = c.fibonacciNumberFinder(l);
-        }
-        else if (op.equals("divide")) {
-            answer = c.divide(l,r);
-        } // More comments
-        else if (op.equals("multiply")) {
-            answer = c.multiply(l, r);
-        }
-        else if (op.equals("bin")) {
-            System.out.println(c.intToBinaryNumber(l));
-            return;
-        }
-        else {
-            System.out.println("Bad idea man.");
-            return;
-        }
-        System.out.println("" + answer);
     }
 }
